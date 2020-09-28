ae37f98c2e1b79d9562b0fde46498784f16aba69
Ethan Mann <emannguitar@gmail.com>
2020-09-28 01:16:20 -0400

    Basic project structure/dependencies

---
+++ json2dataclass/cli.py
@@ -0,0 +1,10 @@
+from typing import Union, Optional
+from pathlib import Path
+import typer
+
+app = typer.Typer()
+
+# TODO: Select dataclass lib
+# TODO: File or dir (to build a single file/module respectively)
+# TODO: Custom output location
+# TODO: Overrides
